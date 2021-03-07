b607b817dfeeb92d21faee699035d2f4caea9d2e
Diego Rodrigues <diego.mrodrigues11@gmail.com>
2021-03-07 14:49:15 -0300

    Create find_median_merge_sort.py

---
+++ alg/find_median_merge_sort.py
@@ -0,0 +1,45 @@
+
+def merge_sort(arr, st, end):
+  if end - st < 2:
+    return
+  
+  mid = (end + st) // 2
+
+  merge_sort(arr, st, mid)
+  merge_sort(arr, mid, end)
+
+  merge(arr, st, mid, end)
+
+def merge(arr, st, mid, end):
+  if arr[mid - 1] <= arr[mid]:
+    return
+  
+  i = st
+  j = mid
+
+  temp_idx = 0
+  temp_arr = [None]*(end - st)
+
+  while i < mid and j < end:
+    if arr[i] <= arr[j]:
+      temp_arr[temp_idx] = arr[i]
+      i += 1
+    else:
+      temp_arr[temp_idx] = arr[j]
+      j += 1
+
+    temp_idx += 1
+
+  for k in range(i, st + temp_idx):
+    arr[(mid - i) + k] = arr[k]
+
+  for k in range(0, temp_idx):
+    arr[st + k] = temp_arr[k]
+
+
+def findMedian(arr):
+  merge_sort(arr, 0, len(arr))
+
+  mid = len(arr) // 2
+
+  return arr[mid]
